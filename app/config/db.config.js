8a6476b4bcfc84fdc6431fd71a6651c9df135b6d
Andrea Romagnoli <a.romagnoli@rvlsolution.it>
2022-05-11 17:41:43 +0200

    First Project test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: "lhcp3134.webapps.net",
+  USER: "ps46zueb_pres549",
+  PASSWORD: "MonteMorello22",
+  DB: "ps46zueb_pres549",
 };
